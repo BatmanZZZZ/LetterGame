178704e818b406d6206bbe02923904fde8b3b5b1
BatmanZZZZ <bsef20m525@pucit.edu.pk>
2023-07-08 14:48:57 +0500

    Final

--- app/src/main/java/com/example/letterplay/MyFragment.java
+++ app/src/main/java/com/example/letterplay/MyFragment.java
@@ -41,168 +41,203 @@ public class MyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_my, container, false);
 
-        letterTextView = view.findViewById(R.id.letter_text_view);
-        letterTextView.setText(getRandomLetter());
+        try {
+            letterTextView = view.findViewById(R.id.letter_text_view);
+            letterTextView.setText(getRandomLetter());
 
-        answerTextView = view.findViewById(R.id.answer_text_view);
+            answerTextView = view.findViewById(R.id.answer_text_view);
 
-        Button skyButton = view.findViewById(R.id.sky_button);
-        skyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String selectedButton = "Sky";
-                if (answerString.equals("Sky Letter")) {
-                    answerTextView.setText("Awesome, your answer is correct");
-                    String modified= answerString.replaceAll("Letter","");
-                    correctAnswers[questionCount] = modified;
-                    score++;
-                } else {
-                    answerTextView.setText("Incorrect! The answer is " + answerString);
-                    String modified= answerString.replaceAll("Letter","");
-                    correctAnswers[questionCount] = modified;
-                }
+            Button skyButton = view.findViewById(R.id.sky_button);
+            skyButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        String selectedButton = "Sky";
+                        if (answerString.equals("Sky Letter")) {
+                            answerTextView.setText("Awesome, your answer is correct");
+                            String modified = answerString.replaceAll("Letter", "");
+                            correctAnswers[questionCount] = modified;
+                            score++;
+                        } else {
+                            answerTextView.setText("Incorrect! The answer is " + answerString);
+                            String modified = answerString.replaceAll("Letter", "");
+                            correctAnswers[questionCount] = modified;
+                        }
 
-                selections[questionCount] = selectedButton;
+                        selections[questionCount] = selectedButton;
 
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        proceedToNextQuestionWithDelay();
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                proceedToNextQuestionWithDelay();
+                            }
+                        }, 2000);
+                    } catch (Exception e) {
+                        Toast.makeText(getActivity(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
-                }, 2000);
-            }
-        });
-
-        Button grassButton = view.findViewById(R.id.grass_button);
-        grassButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String selectedButton = "Grass";
-                if (answerString.equals("Grass Letter")) {
-                    answerTextView.setText("Awesome, your answer is correct");
-                    String modified= answerString.replaceAll("Letter","");
-                    correctAnswers[questionCount] = modified;
-                    score++;
-                } else {
-                    answerTextView.setText("Incorrect! The answer is " + answerString);
-                    String modified= answerString.replaceAll("Letter","");
-                    correctAnswers[questionCount] = modified;
                 }
+            });
 
-                selections[questionCount] = selectedButton;
+            Button grassButton = view.findViewById(R.id.grass_button);
+            grassButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        String selectedButton = "Grass";
+                        if (answerString.equals("Grass Letter")) {
+                            answerTextView.setText("Awesome, your answer is correct");
+                            String modified = answerString.replaceAll("Letter", "");
+                            correctAnswers[questionCount] = modified;
+                            score++;
+                        } else {
+                            answerTextView.setText("Incorrect! The answer is " + answerString);
+                            String modified = answerString.replaceAll("Letter", "");
+                            correctAnswers[questionCount] = modified;
+                        }
 
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        proceedToNextQuestionWithDelay();
+                        selections[questionCount] = selectedButton;
+
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                proceedToNextQuestionWithDelay();
+                            }
+                        }, 2000);
+                    } catch (Exception e) {
+                        Toast.makeText(getActivity(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
-                }, 2000);
-            }
-        });
-
-        Button rootButton = view.findViewById(R.id.root_button);
-        rootButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String selectedButton = "Root";
-                if (answerString.equals("Root Letter")) {
-                    answerTextView.setText("Awesome, your answer is correct");
-                    String modified= answerString.replaceAll("Letter","");
-                    correctAnswers[questionCount] = modified;
-                    score++;
-                } else {
-                    answerTextView.setText("Incorrect! The answer is " + answerString);
-                    String modified= answerString.replaceAll("Letter","");
-                    correctAnswers[questionCount] = modified;
                 }
+            });
 
-                selections[questionCount] = selectedButton;
+            Button rootButton = view.findViewById(R.id.root_button);
+            rootButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        String selectedButton = "Root";
+                        if (answerString.equals("Root Letter")) {
+                            answerTextView.setText("Awesome, your answer is correct");
+                            String modified = answerString.replaceAll("Letter", "");
+                            correctAnswers[questionCount] = modified;
+                            score++;
+                        } else {
+                            answerTextView.setText("Incorrect! The answer is " + answerString);
+                            String modified = answerString.replaceAll("Letter", "");
+                            correctAnswers[questionCount] = modified;
+                        }
 
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        proceedToNextQuestionWithDelay();
+                        selections[questionCount] = selectedButton;
+
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                proceedToNextQuestionWithDelay();
+                            }
+                        }, 1000);
+                    } catch (Exception e) {
+                        Toast.makeText(getActivity(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
-                }, 1000);
-            }
-        });
+                }
+            });
 
-        databaseHelper = new DatabaseHelper(getActivity());
+            databaseHelper = new DatabaseHelper(getActivity());
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
 
         return view;
     }
 
     private void proceedToNextQuestionWithDelay() {
-        if (questionCount < 5) {
-            questions[questionCount] = letterTextView.getText().toString(); // Save the current question
-            questionCount++;
-            answerTextView.setText("");
+        try {
+            if (questionCount < 5) {
+                questions[questionCount] = letterTextView.getText().toString(); // Save the current question
+                questionCount++;
+                answerTextView.setText("");
 
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    proceedToNextQuestion();
-                }
-            }, 2000);
-        } else {
-            saveTestResult();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        proceedToNextQuestion();
+                    }
+                }, 2000);
+            } else {
+                saveTestResult();
+            }
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
 
     private void proceedToNextQuestion() {
-        if (questionCount < 5) {
-            letterTextView.setText(getRandomLetter());
-        } else {
-            saveTestResult();
+        try {
+            if (questionCount < 5) {
+                letterTextView.setText(getRandomLetter());
+            } else {
+                letterTextView.setText(" ");
+                saveTestResult();
+            }
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
 
     private void saveTestResult() {
-        // Concatenate the questions array into a single string
-        String questionsString = Arrays.toString(questions);
+        try {
+            // Concatenate the questions array into a single string
+            String questionsString = Arrays.toString(questions);
 
-        // Remove the brackets from the string
-        questionsString = questionsString.substring(1, questionsString.length() - 1);
+            // Remove the brackets from the string
+            questionsString = questionsString.substring(1, questionsString.length() - 1);
 
-        // Concatenate the selections array into a single string
-        String selectionsString = Arrays.toString(selections);
+            // Concatenate the selections array into a single string
+            String selectionsString = Arrays.toString(selections);
 
-        // Remove the brackets from the string
-        selectionsString = selectionsString.substring(1, selectionsString.length() - 1);
+            // Remove the brackets from the string
+            selectionsString = selectionsString.substring(1, selectionsString.length() - 1);
 
-        // Concatenate the correctAnswers array into a single string
-        String correctAnswersString = Arrays.toString(correctAnswers);
+            // Concatenate the correctAnswers array into a single string
+            String correctAnswersString = Arrays.toString(correctAnswers);
 
-        // Remove the brackets from the string
-        correctAnswersString = correctAnswersString.substring(1, correctAnswersString.length() - 1);
+            // Remove the brackets from the string
+            correctAnswersString = correctAnswersString.substring(1, correctAnswersString.length() - 1);
 
-        databaseHelper.addTestResult(questionsString, selectionsString, correctAnswersString, score);
+            databaseHelper.addTestResult(questionsString, selectionsString, correctAnswersString, score);
 
-        questionCount = 0;
-        score = 0;
+            questionCount = 0;
+            score = 0;
 
-        letterTextView.setText("Test Completed");
-        Toast.makeText(getActivity(), "Test Completed! Results saved in the database.", Toast.LENGTH_SHORT).show();
+            letterTextView.setText("Test Completed");
+            Toast.makeText(getActivity(), "Test Completed! Results saved in the database.", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
     private String getRandomLetter() {
-        Random random = new Random();
-        int category = random.nextInt(3);
-        char letter;
-        switch (category) {
-            case 0:
-                letter = skyLetters[random.nextInt(skyLetters.length)];
-                answerString = "Sky Letter";
-                break;
-            case 1:
-                letter = grassLetters[random.nextInt(grassLetters.length)];
-                answerString = "Grass Letter";
-                break;
-            default:
-                letter = rootLetters[random.nextInt(rootLetters.length)];
-                answerString = "Root Letter";
-                break;
+        try {
+            Random random = new Random();
+            int category = random.nextInt(3);
+            char letter;
+            switch (category) {
+                case 0:
+                    letter = skyLetters[random.nextInt(skyLetters.length)];
+                    answerString = "Sky Letter";
+                    break;
+                case 1:
+                    letter = grassLetters[random.nextInt(grassLetters.length)];
+                    answerString = "Grass Letter";
+                    break;
+                default:
+                    letter = rootLetters[random.nextInt(rootLetters.length)];
+                    answerString = "Root Letter";
+                    break;
+            }
+            return String.valueOf(letter);
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), "" + e.getMessage(), Toast.LENGTH_SHORT).show();
         }
-        return String.valueOf(letter);
+
+        return null;
     }
 
     // Rest of your code
